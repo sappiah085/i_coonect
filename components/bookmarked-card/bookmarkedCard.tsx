fc7a9767f4b3859f7cb9f8bc15bbdcdb0fe8b688
realmann <sappiah085@st.ug.edu.gh>
2023-04-11 09:12:59 +0200

    changes

--- components/bookmarked-card/bookmarkedCard.tsx
+++ components/bookmarked-card/bookmarkedCard.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { AiFillStar } from "react-icons/ai";
 const BookmarkedCard = ({ image }: { image: string }) => {
   return (
-    <article className="flex w-full lg:mr-6  gap-3">
-      <div className="md:w-[200px] flex-shrink-0 w-[180px]">
+    <article className="flex w-[24rem] flex-shrink-0 lg:mr-6 mr-2  gap-3">
+      <div className="md:w-[150px] flex-shrink-0 w-[180px]">
         <Image
           className="object-cover w-full h-full rounded-md "
           src={image}
@@ -13,15 +13,15 @@ const BookmarkedCard = ({ image }: { image: string }) => {
           alt="movie alt"
         />
       </div>
-      <div className="flex flex-col justify-between  max-w-sm">
+      <div className="flex flex-col flex-shrink-0 justify-between w-[200px]">
         <h3 className="text-lg lg:text-xl text-deep_blue font-semibold">
           Till I Break
         </h3>
-        <p className="text-grey">
+        <p className="text-grey break-words">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue lacus,
           scelerisque pretium consectetur. In vitae eu faucibus vel.
         </p>
-        <div className="flex gap-2 items-center w-full text-deep_blue font-semibold text-sm">
+        <div className="flex flex-wrap gap-2 items-center w-full text-deep_blue font-semibold text-sm">
           <span>Movie</span> <span>Action</span> <span>2024</span>{" "}
           <span className="flex items-center">
             <span className="text-[#F3CE13]">
